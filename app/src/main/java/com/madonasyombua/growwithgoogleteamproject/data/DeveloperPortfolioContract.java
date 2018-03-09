97743995f63cc35024fdb42267b4ad6aa07b2a50
Rado RAMBININTSOA <rambininra@gmail.com>
2018-03-09 06:24:25 -0500

    Added database contract.

---
+++ app/src/main/java/com/madonasyombua/growwithgoogleteamproject/data/DeveloperPortfolioContract.java
@@ -0,0 +1,31 @@
+package com.madonasyombua.growwithgoogleteamproject.data;
+
+
+import android.provider.BaseColumns;
+
+public class DeveloperPortfolioContract {
+
+    // TODO: add Users table
+    public class UserEntry implements BaseColumns {
+
+        public static final String TABLE_USER           = "users";
+        public static final String COLUMN_FIRSTNAME     = "firstName";
+        public static final String COLUMN_LASTNAME      = "lastName";
+        public static final String COLUMN_EMAIL         = "firstName";
+        public static final String COLUMN_CREATED       = "created";
+
+
+    }
+
+    // TODO: add Feeds table
+    public class FeedEntry implements BaseColumns {
+
+        public static final String TABLE_FEED           = "feeds";
+        public static final String COLUMN_DESCRIPTION   = "description";
+        public static final String COLUMN_USER          = "user";
+        public static final String COLUMN_CREATED       = "created";
+
+    }
+
+    // TODO: add more tables as needed here
+}
